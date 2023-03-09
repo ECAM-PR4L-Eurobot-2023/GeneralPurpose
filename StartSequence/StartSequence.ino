bea1d9723b93e0e1d4add195902fddf4449268d5
olingo99 <engels.theo@skynet.be>
2023-03-09 12:00:46 +0100

    added screen class

--- StartSequence/StartSequence.ino
+++ StartSequence/StartSequence.ino
@@ -1,6 +1,5 @@
 #include <Arduino.h>
-#include <U8g2lib.h>
-#include <Wire.h>
+
 #include "src/StartPlateSelector.h"
 
 #include "src/data/displacement.h"
@@ -8,11 +7,15 @@
 #include "src/ros_api/ros_api.h"
 #include "src/ros_api/topics.h"
 
+#include "src/screen.h"
 
 
-#define Potar 34
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
+#define potar 34
+
+#define startCord
+
+Screen screen;
 
 RosApiCallbacks callbacks {};
 RosApi *rosApi;
@@ -21,10 +24,10 @@ int count = 0;
 int prevCount = 0;
 boolean drawn = false;
 
-StartPlateSelector startPlateSelector(Potar);
+StartPlateSelector startPlateSelector(potar);
 
 void setup(void) {
-    u8g2.begin();
+    screen.begin();
     startPlateSelector.begin();
     Serial.begin(115200);
 }
@@ -37,11 +40,6 @@ void loop(void) {
     }
 
   if(!drawn){
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_inr24_mn);
-    char buf[4];
-    sprintf(buf, "%d", count);
-    u8g2.drawStr(70-(20* ((count>9? 1: 0)+(count>99? 1: 0))),44,buf);
-    u8g2.sendBuffer();
+    screen.draw(count);
   }
 }
---
+++ StartSequence/src/Screen.cpp
@@ -0,0 +1,19 @@
+#include "Screen.h"
+
+
+Screen::Screen() : display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE)
+{}
+
+void Screen::begin()
+{
+    display.begin();
+}
+
+void Screen::draw(int count){
+    display.clearBuffer();
+    display.setFont(u8g2_font_inr24_mn);
+    char buf[4];
+    sprintf(buf, "%d", count);
+    display.drawStr(70-(20* ((count>9? 1: 0)+(count>99? 1: 0))),44,buf);
+    display.sendBuffer();
+}
